f328a9a055bc27e656b40dfe991212984913f898
Aniuchi Adaobi Michelle <adamichelllle@gmail.com>
2019-09-19 22:21:17 +0100

    Minor changes

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import Widget from './Widget';
 const el = document.getElementById('medium-posts-widget');
 const rssFeedLink = el.getAttribute('data-medium-rss')
 ReactDOM.render(<Widget mediumRSSFeedLink={rssFeedLink} />, el);
-;
+
