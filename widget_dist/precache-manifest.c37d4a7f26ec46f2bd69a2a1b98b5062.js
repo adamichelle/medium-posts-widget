2b72616bb112f554cefdb0451214c4593321a675
Aniuchi Adaobi Michelle <adamichelllle@gmail.com>
2019-09-19 23:10:51 +0100

    Add configuration and custom build scripts

---
+++ widget_dist/precache-manifest.c37d4a7f26ec46f2bd69a2a1b98b5062.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "885ba899a34fc3eec7a4",
+    "url": "/static/js/widget.js"
+  },
+  {
+    "revision": "885ba899a34fc3eec7a4",
+    "url": "/static/css/widget.css"
+  },
+  {
+    "revision": "ddf767c986405a41d56538aa8ef3054c",
+    "url": "/index.html"
+  }
+];
